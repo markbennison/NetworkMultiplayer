ffae5a38c6cbe2266e39c1a1439a344ff8a6570b
Mark Bennison <mark.bennison@live.co.uk>
2020-04-14 15:30:57 +0100

    Basic spawning implemented, not across network.
    Basic spawning is implemented, but needs changes to implement across network. (Stage #1)

---
+++ Assets/Scripts/PlayerObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class PlayerObject : NetworkBehaviour
+{
+    public GameObject playerUnitPrefab;
+
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            // Another player, do nothing
+            return;
+        }
+
+        // Create instance on local machine
+        Instantiate(playerUnitPrefab);
+    }
+
+    void Update()
+    {
+        
+    }
+}
