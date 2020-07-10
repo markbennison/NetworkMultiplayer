cac7b55307349666523bde2a9fb1718288f6cbfa
Mark Bennison <mark.bennison@live.co.uk>
2020-07-10 14:09:29 +0100

    Client-side lifebar created that links to player health
    Server does not update client health bars.

---
+++ Assets/Scripts/HPManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.Networking;
+public class HPManager : NetworkBehaviour
+{
+    public GameObject panelCurrentHP;
+    private RectTransform lifeBarRectTransform;
+    public float currentHP;
+    public float maxHP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHP = 100;
+        currentHP = 100;
+        //if (lifeBarRectTransform != null)
+        //{
+            lifeBarRectTransform = (RectTransform)panelCurrentHP.transform;
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (lifeBarRectTransform != null)
+        //{
+            lifeBarRectTransform.sizeDelta = new Vector2(currentHP, lifeBarRectTransform.sizeDelta.y);
+        //} 
+    }
+}
---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerData : NetworkBehaviour
+{
+    public float MaxHP;
+    public float CurrentHP;
+    public HPManager myHPManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MaxHP = 100;
+        CurrentHP = 100;
+        foreach (Transform eachChild in transform)
+        {
+            if (eachChild.name == "Canvas")
+            {
+                myHPManager = eachChild.GetComponent<HPManager>();
+            }
+        }
+        //myHPManager = GameObject.FindWithTag("Canvas").GetComponent<HPManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasAuthority)
+        {
+            myHPManager.currentHP = CurrentHP;
+        }
+    }
+}
