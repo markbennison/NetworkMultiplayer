63a34924c72073c47d2c8557fa7211ecf933f4b4
Mark Bennison <mark.bennison@live.co.uk>
2020-08-06 15:50:31 +0100

    Bug Fix: Player 2 getting twice the ammo on pickup
    Applied additional ammo as server call and removed pickup using RPC.

--- Assets/Scripts/AmmoPickupController.cs
+++ Assets/Scripts/AmmoPickupController.cs
@@ -15,9 +15,37 @@ public class AmmoPickupController : NetworkBehaviour
     {
         if (collidedObject.tag == "Player")
         {
-            CharacterStates characterStates = collidedObject.GetComponent<CharacterStates>();
-            characterStates.CmdIncreaseAmmo(collidedObject.name, ammoAmount);
-            Destroy(this.gameObject);
+            //CharacterStates characterStates = collidedObject.GetComponent<CharacterStates>();
+            //characterStates.CmdIncreaseAmmo(collidedObject.name, ammoAmount);
+            //Destroy(this.gameObject);
+
+            CmdPickupAction(collidedObject.gameObject, this.gameObject);
         }
+    }
+
+    /* ********** *************** ********** */
+    /* ********** SERVER COMMANDS ********** */
+    /* ********** *************** ********** */
+
+    [Command]
+    public void CmdPickupAction(GameObject player, GameObject pickup)
+    {
+        //PlayerManager player = GameManager.GetPlayer(playerID);
+        //player.gameObject.GetComponent<CharacterStates>().RpcTriggerJump();
+
+        CharacterStates characterStates = player.GetComponent<CharacterStates>();
+        characterStates.CmdIncreaseAmmo(player.name, ammoAmount);
+        
+        RpcPickupAction(pickup);
+    }
+
+    /* ********** *************** ********** */
+    /* ***** SERVER-TO-CLIENT COMMANDS ***** */
+    /* ********** *************** ********** */
+
+    [ClientRpc]
+    public void RpcPickupAction(GameObject pickup)
+    {
+        Destroy(pickup.gameObject);
     }
 }
